42dd5807c7408c42626055d589d204fc345938d1
adventurer.programmer@gmail.com <adventurer.programmer@gmail.com>
2018-08-04 00:34:50 +0200

    adding structure

---
+++ src/CSharpCodeGenerator/Infrastructure/Interface/IVSFileManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeGenerator.Infrastructure.Interface
+{
+    public interface IFileManagement 
+    {
+        public void Add();
+        public void Remove();
+    }
+}
--- src/CSharpCodeGenerator/Program.cs
+++ src/CSharpCodeGenerator/Program.cs
@@ -10,6 +10,12 @@ namespace CSharpCodeGenerator
     {
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }          
+            Console.ReadKey();
         }
     }
 }
+
